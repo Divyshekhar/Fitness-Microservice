09a063ebd70e142d965e0c3217a213d5602eafc2
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2026-06-19 22:50:55 +0530

    rabbitmq listener

---
+++ aiservice/src/main/java/com/fitness/aiservice/config/RabbitmqConfig.java
@@ -0,0 +1,30 @@
+package com.fitness.aiservice.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+    @Bean
+    public Queue activityQueue(){
+        return new Queue("activity.queue", true);
+    }
+    @Bean
+    public DirectExchange activityExchange(){
+        return new DirectExchange("fitness.exchange");
+    }
+    @Bean
+    public Binding activityBinding(Queue activityQueue, DirectExchange activityExchange){
+        return BindingBuilder.bind(activityQueue).to(activityExchange).with("activity.tracking");
+    }
+
+    @Bean
+    public JacksonJsonMessageConverter jsonMessageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+}
