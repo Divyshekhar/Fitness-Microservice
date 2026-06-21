a8f5dc7a215f6b8dd3f4f5875139cb23685514a1
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2026-06-21 11:40:24 +0530

    ai parsing and saving in db & config server init

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,6 +1,8 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -11,11 +13,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ActivityMessageListener {
     private final ActivityAIService activityAIService;
+    private final RecommendationRepository recommendationRepository;
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
         try{
             log.info("Received activity for processing: {}", activity.getId());
-            log.info("Generated Recommendation: {}", activityAIService.generateRecommendation(activity));
+//            log.info("Generated Recommendation: {}", activityAIService.generateRecommendation(activity));
+            Recommendation recommendation = activityAIService.generateRecommendation(activity);
+            recommendationRepository.save(recommendation);
         }catch(Exception e){
             log.error("Error occurred in processing activity in queue", e);
         }
